e70c1d26bf4d544978884ee72bf09fbac0fb0b17
IgorPalchuk <pal4uk.igor@gmail.com>
2024-02-14 20:56:40 +0200

    Task3 Udemy

---
+++ Tasks/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a whole number: ");
+            string number = Console.ReadLine();
+            int quantity = number.Length;
+
+            Console.Write($"Number of digits: {quantity}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
