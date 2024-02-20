09dd0e8988de27f76ffdb2f9554bb19edbe99f1d
IgorPalchuk <pal4uk.igor@gmail.com>
2024-02-20 15:59:06 +0200

    Task 3.1 Udemy(Control FLow)

---
+++ Tasks/Task3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write first number:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Write second number:");
+            int b = int.Parse(Console.ReadLine());
+
+            string max = a > b ? $"Max: {a}" : $"Max: {b}";
+
+            Console.WriteLine(max);
+
+            Console.ReadLine();
+
+        }
+    }
+}
