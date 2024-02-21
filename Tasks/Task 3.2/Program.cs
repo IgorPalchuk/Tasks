b6b60358718a187e888581331edd60db26fe3c45
IgorPalchuk <pal4uk.igor@gmail.com>
2024-02-21 11:56:50 +0200

    Task 3.2 Udemy

---
+++ Tasks/Task 3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers?");
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            int numbers = 1;
+            int[]fibo = new int[numbersCount];
+            fibo[0] = numbers;
+            fibo[1] = numbers;
+            for(int i = 2; i < numbersCount; i++)
+            {
+                fibo[i] = fibo[i - 1] + fibo[i - 2];
+            }
+            for(int i = 0;i < fibo.Length; i++)
+            {
+                Console.WriteLine(fibo[i]);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
