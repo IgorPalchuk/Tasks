3e14b1f5101ed1a457e65a4a4dc8f53ead985272
IgorPalchuk <pal4uk.igor@gmail.com>
2024-02-22 13:28:12 +0200

    Task3.5 Udemy

--- Tasks/Task3.4/Program.cs
+++ Tasks/Task3.4/Program.cs
@@ -10,30 +10,39 @@ namespace Task3._4
     {
         static void Main(string[] args)
         {
+            /*  Console.Write("Write number: ");
+              int number = int.Parse(Console.ReadLine());
+              int fact = 1;
+
+              if (number == 0 || number == 1)
+              {
+                  fact = 1;
+              }
+              else
+              {
+                  int[] digits = new int[number];
+                  digits[0] = number;
+                  for (int i = 1; i < number; i++)
+                  {
+                      digits[i] = digits[i-1] - 1;
+                  }
+
+                  foreach (int i in digits)
+                  {
+                      fact *= i;
+                  }
+              }
+              Console.WriteLine($"!{number} = {fact}");
+              Console.ReadLine();*/
+
             Console.Write("Write number: ");
             int number = int.Parse(Console.ReadLine());
             int fact = 1;
-            
-            if (number == 0 || number == 1)
+            for(int i = 1; i <= number; i++)
             {
-                fact = 1;
+                fact *= i;
             }
-            else
-            {
-                int[] digits = new int[number];
-                digits[0] = number;
-                for (int i = 1; i < number; i++)
-                {
-                    digits[i] = digits[i-1] - 1;
-                }
-
-                foreach (int i in digits)
-                {
-                    fact *= i;
-                }
-            }
-            Console.WriteLine($"!{number} = {fact}");
-            Console.ReadLine();
+            Console.WriteLine(fact);
 
         }
     }
---
+++ Tasks/Task3.5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctLogin = "johnsilver";
+            string correctPassword = "qwerty";
+
+            
+
+            int counter = 0;
+            while(counter < 3)
+            {
+                Console.Write("Write a login: ");
+                string login = Console.ReadLine();
+
+                Console.Write("Write a password: ");
+                string password = Console.ReadLine();
+
+                if (login.Equals(correctLogin) && password.Equals(correctPassword))
+                {
+                    Console.WriteLine("Enter the System");
+                    break;
+                }
+                else
+                {
+                    counter++;
+                }
+
+            }
+            if (counter == 3)
+            { 
+                Console.WriteLine("The number of available tries have been exceeded");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
