c39c450a3f6f529de880328082f9ed872e3a4e8b
IgorPalchuk <pal4uk.igor@gmail.com>
2024-03-14 15:07:52 +0200

    TaskList2.2

---
+++ Tasks/TaskList2.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "121";
+            Console.WriteLine(str.Extension()); 
+        }
+         
+       
+    }
+    public static class StringExtension
+    {
+        public static bool Extension(this string s)
+        {
+            char[] chars = s.ToCharArray();
+            chars.Reverse();
+            string result = new string(chars);
+
+            return s == result;
+;
+        }
+    }
+}
