5cfda8d90dc7fdf49635dffd3a75691f9a2ab72b
IgorPalchuk <pal4uk.igor@gmail.com>
2024-03-14 14:34:02 +0200

    TaskList2.1

---
+++ Tasks/TaskList2.1/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList2._1
+{
+    public class Calculator
+    {
+        private static string Calc;
+         static Calculator() 
+        {
+            Calc = "This is calculator";
+            Console.WriteLine(Calc);
+        }
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;   
+        }
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Divide(double a, double b)
+        {
+            if(b == 0)
+            {
+                throw new ArgumentException("Fail to divide by 0");
+            }
+            else
+            {
+                return a / b;
+            }
+            
+        }
+    }
+}
---
+++ Tasks/TaskList2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(10, 5)); 
+
+            
+             
+        }
+    }
+}
