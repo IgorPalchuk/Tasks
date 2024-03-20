a883a3a2cc409fc4df17c1f01533707e18d7f98e
IgorPalchuk <pal4uk.igor@gmail.com>
2024-03-20 12:16:09 +0200

    Task8.1

---
+++ Tasks/Task8.1/Complex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8._1
+{
+    public class Complex
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+        public override string ToString()
+        {
+            return $"Complex = {Real} + {Imaginary}";
+        }
+        public Complex(double real, double imaginary) 
+        { 
+            this.Real = real;
+            this.Imaginary = imaginary;
+        }
+        public Complex Plus(Complex complex)
+        {
+            double newReal = complex.Real;
+            double newImaginary = complex.Imaginary;
+            double finalReal = newReal + Real;
+            double finalImaginary = newImaginary + Imaginary;
+            Complex result = new Complex(finalReal, finalImaginary);
+            return result;
+        }
+        public Complex Minus(Complex complex)
+        {
+            double newReal = complex.Real;
+            double newImaginary = complex.Imaginary;
+            double finalReal = newReal - Real;
+            double finalImaginary = newImaginary - Imaginary;
+           Complex result = new Complex(finalReal, finalImaginary);
+            return result;
+        }
+    }
+}
---
+++ Tasks/Task8.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(1, 1);
+            Complex c2 = new Complex(1, 2);
+
+            Complex result = c1.Plus(c2);
+            Complex result2 = c2.Minus(c1);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+        }
+    }
+}
