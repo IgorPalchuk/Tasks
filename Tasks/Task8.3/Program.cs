8a3e46f48915081973732cac0848b09ed95fe917
IgorPalchuk <pal4uk.igor@gmail.com>
2024-03-24 21:06:44 +0200

    Task8.3

---
+++ Tasks/Task8.3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8._3
+{
+    internal class Program
+    {
+        private static TicTacToe g = new TicTacToe();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPrintableState());
+            while(g.GetWinner() == Winner.GameIsUnfinished)
+            {
+                int index = int.Parse(Console.ReadLine());
+                g.MakeMove(index);
+
+                Console.WriteLine();
+                Console.WriteLine(GetPrintableState());
+
+            }
+            Console.WriteLine($"Result: {g.GetWinner()}");
+
+        }
+        static string GetPrintableState()
+        {
+            var sb = new StringBuilder();
+            for(int i = 1; i <= 7; i += 3)
+            {
+                sb.AppendLine("     |     |     |")
+                    .AppendLine(
+                    $"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  |")
+                    .AppendLine("_____|_____|_____|");
+            }
+            return sb.ToString();
+        }
+        static string GetPrintableChar(int index)
+        {
+            State state = g.GetState(index);
+            if(state == State.Cross)
+            {
+                return "X";
+            }
+            else if(state == State.Zero)
+            {
+                return "O";
+            }
+            return index.ToString();
+        }
+    }
+}
---
+++ Tasks/Task8.3/TicTacToe.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8._3
+{
+    public enum Winner
+    {
+        Crosses,
+        Zeroes,
+        Draw,
+        GameIsUnfinished
+    }
+    public enum State
+    {
+        Cross,
+        Zero,
+        Unset
+    }
+    public class TicTacToe
+    {
+        private readonly State[] board = new State[9];
+        public int MovesCounter { get; private set; }
+
+        public TicTacToe()
+        { 
+            for(int i = 0; i < board.Length; i++)
+            {
+                board[i] = State.Unset;
+            }
+        }
+
+       public void MakeMove(int index)
+        {
+            board[index - 1] = MovesCounter % 2 == 0 ? State.Cross : State.Zero;
+            MovesCounter++;
+        }
+        public State GetState(int index)
+        {
+            return board[index - 1];
+        }
+        public Winner GetWinner()
+        {
+            return GetWinner(1, 4, 7, 2, 5, 8, 3, 6, 9,
+                1, 2, 3, 4, 5, 6, 7, 8, 9,
+                1, 5, 9, 3, 5, 7);
+        }
+
+        private Winner GetWinner(params int[] indexes)
+        {
+            for(int i =0; i < indexes.Length; i+=3)
+            {
+                bool Same = AreSame(indexes[i], indexes[i + 1], indexes[i + 2]);
+                if(Same)
+                {
+                    State state = GetState(indexes[i]);
+                    if(state != State.Unset)
+                    {
+                        return state == State.Cross ? Winner.Crosses : Winner.Zeroes;
+                    }
+                    
+                }
+
+            }
+            if(MovesCounter < 9)
+            {
+                return Winner.GameIsUnfinished;
+            }
+            return Winner.Draw;
+        }
+        public bool AreSame(int a, int b, int c)
+        {
+            return GetState(a) == GetState(b) && GetState(c) == GetState(a);
+        }
+    }
+}
