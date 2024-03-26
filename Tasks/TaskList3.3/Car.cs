6c788150ec54a06a1343293372f22082f7da44b9
IgorPalchuk <pal4uk.igor@gmail.com>
2024-03-26 15:39:27 +0200

    Task3.3

---
+++ Tasks/TaskList3.3/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList3._3
+{
+    public class Car : Vehicle
+    {
+        public int NumberOfWheels { get; set; }
+        public Car(string model, string year, double fuelCapacity , int numberOfWheels) : base(model , year , fuelCapacity)
+        {
+            this.NumberOfWheels = numberOfWheels;
+        }
+        public override void Drive()
+        {
+            Console.WriteLine("The car is moving");
+        }
+        public override void Refuel(double fuel)
+        {
+           base.Refuel(fuel);
+        }
+    }
+}
---
+++ Tasks/TaskList3.3/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList3._3
+{
+    public class Vehicle
+    {
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public double FuelCapacity { get; set; }
+        public Vehicle(string model, string year, double fuelCapacity)
+        {
+            this.Model = model;
+            this.Year = year;
+            this.FuelCapacity = fuelCapacity;
+        }
+        public virtual void Drive()
+        {
+            Console.WriteLine("The vehicle is moving");
+        }
+        public virtual void Refuel(double fuel)
+        {
+            FuelCapacity += fuel;
+        }
+    }
+}
