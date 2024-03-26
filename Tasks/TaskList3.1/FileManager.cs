50bed1924579da6aa8f9a3c2ff691d4d44f5afc0
IgorPalchuk <pal4uk.igor@gmail.com>
2024-03-26 15:19:53 +0200

    TaskList3.1

---
+++ Tasks/TaskList3.1/FileManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList3._1
+{
+    public class FileManager
+    {
+       public static void Copy(string source, string destination)
+        {
+            try
+            {
+                FileManager.Copy(source, destination);
+            }
+            catch (OutOfMemoryException ex)
+            {
+                throw new OutOfMemoryException(ex.Message);
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                throw new UnauthorizedAccessException(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Tasks/TaskList3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager.Copy("source", "destination");
+        }
+    }
+}
