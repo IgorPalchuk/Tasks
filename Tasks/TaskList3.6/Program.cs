6a902e26b64175e89b2701bea2eb8ea39ed9b0a3
IgorPalchuk <pal4uk.igor@gmail.com>
2024-04-01 12:47:13 +0300

    TaskList3.6

---
+++ Tasks/TaskList3.6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList3._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new Vector(2, 3);
+            Vector vector2 = new Vector(3, 4);
+            Vector vector3 = new Vector(2, 3);
+            Vector plus = vector1 + vector2;
+            Console.WriteLine(plus);
+            Vector minus = vector1 - vector2;
+            Console.WriteLine(minus);
+           
+
+            bool equals = vector1.Equals(vector2);
+            Console.WriteLine(equals);
+
+            bool equals2 = vector1.Equals(vector3);
+            Console.WriteLine(equals2);
+        }
+    }
+}
---
+++ Tasks/TaskList3.6/Vector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList3._6
+{
+    public class Vector
+    {
+
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X + b.X, a.Y + b.Y);
+        }
+
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.X - b.X, a.Y - b.Y);
+        }
+
+        
+        public override string ToString()
+        {
+            return $"{X}, {Y}";
+        }
+        public override bool Equals(object obj)
+        {
+            var v = (Vector)obj;
+            if (v == null)
+            {
+                return false;
+            }
+            else
+            {
+                return (v.X == this.X) && (v.Y == this.Y);
+            }
+        }
+        
+    }
+}
